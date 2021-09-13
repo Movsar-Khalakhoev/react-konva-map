77d54f349eb8615664ed7bfd35294809c3c81897
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2021-09-13 20:30:05 +0300

    Релизованы первая зона 1 линии

---
+++ src/pages/MnemonicScheme/FirstLine/FirstLine.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FirstZone from "./zones/FirstZone"
+
+const FirstLine = () => {
+  return (
+    <>
+      <FirstZone />
+    </>
+  )
+}
+
+export default FirstLine
---
+++ src/pages/MnemonicScheme/SecondLine/SecondLine.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import FirstZone from "./zones/FirstZone"
+import SecondZone from "./zones/SecondZone"
+import ThirdZone from "./zones/ThirdZone"
+import FourthZone from "./zones/FourthZone"
+import FifthZone from "./zones/FifthZone"
+import SixthZone from "./zones/SixthZone"
+import SeventhZone from "./zones/SeventhZone"
+import EighthZone from "./zones/EighthZone"
+import NinthZone from "./zones/NinthZone"
+
+const SecondLine = () => {
+  return (
+    <>
+      <FirstZone />
+      <SecondZone />
+      <ThirdZone />
+      <FourthZone />
+      <FifthZone />
+      <SixthZone />
+      <SeventhZone />
+      <EighthZone />
+      <NinthZone />
+    </>
+  )
+}
+
+export default SecondLine
