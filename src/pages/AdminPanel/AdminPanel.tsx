c34c4aadbaabba832e4ec63286f14e43c6707ef7
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2021-09-13 10:23:57 +0300

    Исправлена админская панель

--- src/pages/AdminPanel/AdminPanel.tsx
+++ src/pages/AdminPanel/AdminPanel.tsx
@@ -1,29 +1,15 @@
 import React from 'react'
 import styles from './AdminPanel.module.sass'
 import PageLayout from "../../components/PageLayout/PageLayout"
-import Modal from "../../components/Modal/Modal"
 import {Layer, Line, Stage} from "react-konva"
 import {KonvaEventObject} from "konva/lib/Node"
 
 const AdminPanel = () => {
-  const [zones] = React.useState([
-    'Зона 1 Упаковочная машина 3',
-    'Зона 2 Упаковочная машина 2',
-    'Зона 3 Упаковочная машина 1',
-    'Зона 4 Магазин поддонов',
-    'Зона 5 Стрейч-Хууд',
-    'Зона 6 Штабелер'
-  ])
-  const [activeZone, setActiveZone] = React.useState<string | null>(null)
-  const [isModalOpen, setIsModalOpen] = React.useState(false)
   const [lines, setLines] = React.useState<number[][]>([]);
   const isDrawing = React.useRef(false);
-  const activeCamera = React.useRef({cameraTitle: '', lastUpdate: new Date()})
+  const broadcastRef = React.useRef<HTMLDivElement | null>(null)
+  const [broadcastSize, setBroadcastSize] = React.useState({width: 0, height: 0})
 
-  const activateZone = (zone: string) => {
-    setActiveZone(zone)
-    setIsModalOpen(true)
-  }
 
   const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
     isDrawing.current = true;
@@ -55,75 +41,59 @@ const AdminPanel = () => {
     })
   };
 
-  const closeModalHandler = () => {
-    setIsModalOpen(false)
-    setLines([])
-    setActiveZone(null)
-  }
-
   React.useEffect(() => {
-    const activeCameraFromLS = JSON.parse(localStorage.getItem('activeCamera') || 'null')
-    if (activeCameraFromLS.current) {
-      activeCamera.current = activeCameraFromLS
+    if (broadcastRef.current) {
+      setBroadcastSize({
+        width: broadcastRef.current.clientWidth,
+        height: broadcastRef.current.clientHeight
+      })
     }
-
-    const activeCameraListener = setInterval(() => {
-      const activeCameraFromLS = JSON.parse(localStorage.getItem('activeCamera') || 'null')
-      if (activeCameraFromLS && new Date(activeCameraFromLS.lastUpdate) > new Date(activeCamera.current.lastUpdate)) {
-        activeCamera.current = activeCameraFromLS
-        setActiveZone(activeCameraFromLS.cameraTitle)
-        setIsModalOpen(true)
-      }
-    }, 100)
-
-    return () => clearInterval(activeCameraListener)
-  }, [])
+  }, [broadcastRef.current])
 
   return (
     <PageLayout>
-      <div className={styles.zones}>
-        {
-          zones.map(zone => (
-            <div className={styles.zone} key={zone}>
-              <p className={styles.zoneTitle} onClick={() => activateZone(zone)}>{zone}</p>
-            </div>
-          ))
-        }
-      </div>
-      <Modal isOpen={isModalOpen} closeHandler={closeModalHandler}>
-        <p className={styles.modalTitle}>{activeZone}</p>
-        <div className={styles.modalContainer}>
-          <div className={styles.joystickContainer}>
-            <button className={styles.joystickButton}>Влево</button>
-            <button className={styles.joystickButton}>Вправо</button>
+      <p className={styles.title}>камера 123123423</p>
+      <div className={styles.container}>
+        <div className={styles.broadcastContainer}>
+          <div className={styles.broadcast} ref={broadcastRef}>
+            <button className={styles.prevStateButton} onClick={
+              () => setLines(prevState => prevState.slice(0, prevState.length - 1))}>&#x2190;</button>
+            <Stage
+              width={broadcastSize.width}
+              height={broadcastSize.height}
+              onMouseDown={handleMouseDown}
+              onMousemove={handleMouseMove}
+              onMouseUp={handleMouseUp}
+            >
+              <Layer>
+                {lines.map((line, i) => (
+                  <Line
+                    key={i}
+                    points={line}
+                    stroke="red"
+                    strokeWidth={2}
+                    tension={0.5}
+                    lineCap="round"
+                    globalCompositeOperation='source-over'
+                    fill='rgba(255, 0, 0, 0.2)'
+                    closed={i === lines.length - 1 ? !isDrawing.current : true}
+                  />
+                ))}
+              </Layer>
+            </Stage>
           </div>
-          <div className={styles.broadcastContainer}>
-            <div className={styles.broadcast}>
-              <button className={styles.prevStateButton} onClick={
-                () => setLines(prevState => prevState.slice(0, prevState.length - 1))}>&#x2190;</button>
-              <Stage
-                width={500}
-                height={500}
-                onMouseDown={handleMouseDown}
-                onMousemove={handleMouseMove}
-                onMouseUp={handleMouseUp}
-              >
-                <Layer>
-                  {lines.map((line, i) => (
-                    <Line
-                      key={i}
-                      points={line}
-                      stroke="red"
-                      strokeWidth={2}
-                      tension={0.5}
-                      lineCap="round"
-                      globalCompositeOperation='source-over'
-                      fill='rgba(255, 0, 0, 0.2)'
-                      closed={i === lines.length - 1 ? !isDrawing.current : true}
-                    />
-                  ))}
-                </Layer>
-              </Stage>
+        </div>
+        <div className={styles.rightPanel}>
+          <div className={styles.joystickContainer}>
+            <div className={styles.joystickButtons}>
+              <button className={styles.joystickTopArrow}>&#x2191;</button>
+              <button className={styles.joystickBottomArrow}>&#x2193;</button>
+              <button className={styles.joystickLeftArrow}>&#x2190;</button>
+              <button className={styles.joystickRightArrow}>&#x2192;</button>
+              <button className={styles.joystickRotateRight}>&#x27F3;</button>
+              <button className={styles.joystickRotateLeft}>&#x27F2;</button>
+              <button className={styles.joystickZoomIn}>&#x2b;</button>
+              <button className={styles.joystickZoomOut}>&#x2212;</button>
             </div>
           </div>
           <div className={styles.actionsContainer}>
@@ -132,7 +102,59 @@ const AdminPanel = () => {
             <button className={styles.actionsCancel}>Отмена</button>
           </div>
         </div>
-      </Modal>
+      </div>
+      {/*<div className={styles.zones}>*/}
+      {/*  {*/}
+      {/*    zones.map(zone => (*/}
+      {/*      <div className={styles.zone} key={zone}>*/}
+      {/*        <p className={styles.zoneTitle} onClick={() => activateZone(zone)}>{zone}</p>*/}
+      {/*      </div>*/}
+      {/*    ))*/}
+      {/*  }*/}
+      {/*</div>*/}
+      {/*<Modal isOpen={isModalOpen} closeHandler={closeModalHandler}>*/}
+      {/*  <p className={styles.modalTitle}>{activeZone}</p>*/}
+      {/*  <div className={styles.modalContainer}>*/}
+      {/*    <div className={styles.joystickContainer}>*/}
+      {/*      <button className={styles.joystickButton}>Влево</button>*/}
+      {/*      <button className={styles.joystickButton}>Вправо</button>*/}
+      {/*    </div>*/}
+      {/*    <div className={styles.broadcastContainer}>*/}
+      {/*      <div className={styles.broadcast}>*/}
+      {/*        <button className={styles.prevStateButton} onClick={*/}
+      {/*          () => setLines(prevState => prevState.slice(0, prevState.length - 1))}>&#x2190;</button>*/}
+      {/*        <Stage*/}
+      {/*          width={500}*/}
+      {/*          height={500}*/}
+      {/*          onMouseDown={handleMouseDown}*/}
+      {/*          onMousemove={handleMouseMove}*/}
+      {/*          onMouseUp={handleMouseUp}*/}
+      {/*        >*/}
+      {/*          <Layer>*/}
+      {/*            {lines.map((line, i) => (*/}
+      {/*              <Line*/}
+      {/*                key={i}*/}
+      {/*                points={line}*/}
+      {/*                stroke="red"*/}
+      {/*                strokeWidth={2}*/}
+      {/*                tension={0.5}*/}
+      {/*                lineCap="round"*/}
+      {/*                globalCompositeOperation='source-over'*/}
+      {/*                fill='rgba(255, 0, 0, 0.2)'*/}
+      {/*                closed={i === lines.length - 1 ? !isDrawing.current : true}*/}
+      {/*              />*/}
+      {/*            ))}*/}
+      {/*          </Layer>*/}
+      {/*        </Stage>*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
+      {/*    <div className={styles.actionsContainer}>*/}
+      {/*      <button className={styles.actionsCreate}>Создать</button>*/}
+      {/*      <button className={styles.actionsApply}>Применить</button>*/}
+      {/*      <button className={styles.actionsCancel}>Отмена</button>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</Modal>*/}
     </PageLayout>
   )
 }
