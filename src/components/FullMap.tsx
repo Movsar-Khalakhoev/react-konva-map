81a9ee24ec80c482c74f208ad3df39a8e17c55d4
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2021-09-14 11:42:34 +0300

    Сборка проект

--- src/components/FullMap.tsx
+++ src/components/FullMap.tsx
@@ -11,7 +11,7 @@ import SecondLine from "../pages/MnemonicScheme/SecondLine/SecondLine"
 const scaleBy = 1.03;
 
 const FullMap = () => {
-  const {image} = useImage(zonesPlanImage)
+  const {image} = useImage(process.env.NODE_ENV === 'production' ? `${window.location.host}/react-konva-map/build${zonesPlanImage}` : zonesPlanImage)
   const {
     fullMapRef,
     setFullMapXPosition,
